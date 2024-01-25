280ffbc11227bcee27265aa5c003f6e0310140ca
devbwoh <bwoh@outlook.kr>
2024-01-26 04:01:59 +0900

    🎴 Card 사용

--- app/src/main/java/kr/ac/kumoh/ce/prof01/jet15card/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/prof01/jet15card/MainActivity.kt
@@ -3,12 +3,26 @@ package kr.ac.kumoh.ce.prof01.jet15card
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import kr.ac.kumoh.ce.prof01.jet15card.ui.theme.Jet15CardTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,7 +42,43 @@ fun MainScreen() {
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
+        Column {
+            SongItem()
+            SongItem()
+        }
+    }
+}
 
+@Composable
+fun SongItem() {
+    Card(
+        Modifier
+            .padding(8.dp)
+            .fillMaxWidth(),
+        elevation = CardDefaults.cardElevation(8.dp)
+    ) {
+        Row {
+            Box(
+                Modifier.padding(8.dp).clip(CircleShape)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = "노래 앨범 이미지"
+                )
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                    contentDescription = "노래 앨범 이미지"
+                )
+            }
+            Column {
+                Text(
+                    text = "사랑에 연습이 있었다면",
+                    fontSize = 25.sp,
+                    lineHeight = 30.sp,
+                )
+                Text("임재현", fontSize = 20.sp)
+            }
+        }
     }
 }
 
